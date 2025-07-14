f0dd42897aa73dc22475cc3ff23a49f544a9f5bd
sonykikuma <er.abhasoni@gmail.com>
2025-07-14 16:50:57 +0530

    get request message changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/client", async (req, res) => {
     const clients = await Client.find();
     res.status(200).json(clients);
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch inventory" });
+    res.status(500).json({ message: "Failed to fetch student queries" });
   }
 });
 
